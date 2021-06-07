be39696c3f006f3bd90f6d4e7e9c4c8ebe22d536
Clemens Schmid <clemens@nevrome.de>
2021-06-07 15:04:43 +0200

    better comments for the exercise code

--- exercises/exercise_functions.R
+++ exercises/exercise_functions.R
@@ -1,37 +1,62 @@
+#### Generate test data ####
 input_data <- data.frame(
   id = LETTERS[1:26],
-  valueI = runif(26),
-  valueII = runif(26)
+  value_A = runif(26),
+  value_B = runif(26)
 )
+input_data$value_A[sample(1:26, 5)] <- NA
+input_data$value_B[sample(1:26, 5)] <- NA
 
-input_data$valueI[sample(1:26, 5)] <- NA
-input_data$valueII[sample(1:26, 5)] <- NA
+#### What this code does ####
+# This code creates a third column for input_data that combines the
+# two columns value_A and value_B in a somewhat complicated manner
 
-cutoff_letter <- "G"
-
+# create an empty vector to store the loop's result
 loop_res <- rep(NA, nrow(input_data))
+# loop to somehow combine the two value columns
+# for each row of the data.frame
 for (i in seq_len(nrow(input_data))) {
+  # get the row for the current loop cycle
   current_row <- input_data[i,]
-  if (current_row$id > cutoff_letter) {
-    loop_res[i] <- current_row$valueI - current_row$valueII
+  if (current_row$id < "G") {
+    # if the id is smaller then the cutoff-letter "G",
+    # then the combination of value_A and value_B is very simple
+    loop_res[i] <- current_row$value_A - current_row$value_B
   } else {
-    if (is.na(current_row$valueI)) {
-      v1 <- 0
+    # else it gets more complicated, because NA values
+    # have to be replaced
+    if (is.na(current_row$value_A)) {
+      # NA values in value A get replaced with 0
+      v_A <- 0
     } else {
-      v1 <- current_row$valueI
+      # normal values just stay as they are
+      v_A <- current_row$value_A
     }
-    if (is.na(current_row$valueII)) {
-      v2 <- 0.5
+    if (is.na(current_row$value_B)) {
+      # NA values in value B get replaced with 0.5
+      v_B <- 0.5
     } else {
-      v2 <- current_row$valueII
+      # again normal values just stay as they are
+      v_B <- current_row$value_B
     }
-    loop_res[i] <- v1 + v2
+    # finally the two modified values are added
+    loop_res[i] <- v_A + v_B
   }
 }
 
+# result vector
 loop_res
 
-### a possible solution
+#### The task ####
+# Imagine you have to do this for hundreds of data.frames equivalent
+# to input_data. But you have to treat them with a different cutoff_letter
+# each, so not only "G".
+# Can you rewrite this code into one or multiple functions?
+
+
+
+
+#### a possible solution ####
 
 specialplus <- function(x, y) {
   if (is.na(x)) {
@@ -53,14 +78,13 @@ my_function <- function(x, cutoff_letter) {
   loop_res <- rep(NA, nrow(x))
   for (i in seq_len(nrow(x))) {
     current_row <- x[i,]
-    if (current_row$id > cutoff_letter) {
-      loop_res[i] <- current_row$valueI - current_row$valueII
+    if (current_row$id < cutoff_letter) {
+      loop_res[i] <- current_row$value_A - current_row$value_B
     } else {
-      loop_res[i] <- current_row$valueI %specialplus% current_row$valueII
+      loop_res[i] <- current_row$value_A %specialplus% current_row$value_B
     }
   }
   return(loop_res)
 }
 
-my_function(input_data, cutoff_letter)
-
+my_function(input_data, "G")
